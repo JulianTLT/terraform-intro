06f26462410cb7a0ef14d84380c68302d362b973
Julian Sanchez <jsanchezmendez@twilio.com>
2021-03-14 21:56:50 -0500

    ch2 deploy configurable web server

--- main.tf
+++ main.tf
@@ -5,7 +5,25 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami = "ami-4ae27e22"
   instance_type = "t2.micro"
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+  vpc_security_group_ids = [aws_security_group.instance.id]
   tags = {
     Name = "terraform-example"
   }
 }
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port = var.server_port
+    protocol = "tcp"
+    to_port = var.server_port
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
